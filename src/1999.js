f933da28ebc9a75bebf90240ac19962bccc1ef84
jadel449 <77951937+jadel449@users.noreply.github.com>
2021-01-28 09:40:53 -0600

    Update 1999.js

--- src/1999.js
+++ src/1999.js
@@ -156,3 +156,122 @@ export const reg99 ={
 export const state99={
 
 }
+
+export const invA13 = {
+    '511-115=': '396',
+    '88\\times25=': '2200',
+    '2013\\div9=\\textrm{mixed number}': '223 2/3',
+    '115+2013':'2128',
+'\\frac{3}{5}=\\textrm{___\\% (decimal)}': '60',
+    '2.4\\div1.5=': ['1.6','8/5','1 3/5'],
+    '14^2=': '196',
+    '5\\frac{3}{4}-4\\frac{2}{3}=\\textrm{mixed number}': '1 1/12',
+    '11\\%of \\$12.00 is \\$__': '1.32',
+    '115+2013-511+3102=': 4719,
+    '\\textrm{Which is larger }\\frac{7}{9} \\textrm{ or 0.8?} ': '.8',
+    '40\\times 23-17\\times 23': '529',
+    '\\textrm{2 bushels= ___ pecks}': '8',
+    '19\\times \\frac{19}{23}=': '15 16/23',
+    '115\\div25=': ['4.6','23/5','4 3/5'],
+    '\\textrm{The mean of 1,5,12,22,35 is __}': '15',
+    '115\\times 13': '1495',
+    '2+4+6+8+...+22+24=': '156',
+    '32-16\\div8+4\\times2=': '38',
+    '(115\\div2013)\\times511': 1087408
+}
+
+export const invB13 = {
+	'2013-3102=': '-1089',
+	'28\\times15': '420',
+	'310.2+20.13': '330.33',
+	'2\\frac{1}{3}\\div 3\\frac{1}{2}': '2/3',
+	'0.875=\\textrm{proper fraction}': '7/8',
+	'54536\\div 9=': '60504',
+	'6543\\times 9-2': '58885',
+	'25\\times 20-15+10\\div 5=': '487',
+	'\\textrm{2.5 gallons = ___ quarts}': '10',
+	'21347+118+2947+76': 25488,
+	'\\textrm{The GCD of 54, 48, and 32 is:}': '2',
+	'2013\\div 11\\textrm{ has a remainder of}': '0',
+	'\\textrm{CMLXIV}': '964',
+	'32\\times 17+15\\times 32=': '1024',
+	'3+7+11+15+...+31=': '136',
+	'\\frac{15}{19}\\times 15': '11 16/19',
+	'\\textrm{The largest prime factor of 285 is __}': '19',
+	'\\frac{7}{12}-\\frac{7}{24}-\\frac{7}{36}': '7/72',
+	'\\textrm{Which is smaller, -0.45 or }\\frac{4}{9}': ['-.45','-9/20'],
+	'8\\times15\\times 194=': 133640
+}
+
+export const dis14 = {
+	'317+322=': '639',
+	'44\\times 15=': '660',
+	'4102\\div5=': '820.4',
+	'713-223=': '490',
+	'\\frac{6}{7}\\times\\frac{49}{50}=': ['.84', '21/25'],
+	'22\\times 17=': '374',
+	'\\frac{3}{16}=': '.1875',
+	'3\\frac{1}{7}+2\\frac{2}{3}': '5 17/21',
+	'17^2=': '289',
+	'317+2014+322+201+4': 2858,
+	'3\\frac{5}{12}-1\\frac{3}{4}': '1 2/3',
+	'23\\% \\textrm{ of }23=': '5.29',
+	'3+2\\times2\\div3-(1+7)=': ['-11/3','-3 2/3'],
+	'713140\\div\\textrm{ has a remainder of}': '3',
+	'\\textrm{1.5 gallons = ___ pints}': '12',
+	'75\\%\\textrm{ of \\$36.08 = \$____}': '27.06',
+	'\\textrm{CCCLXXI= _____(Arabic Number)}': '371',
+	'\\textrm{LCM of 48 and 84 is}': '336',
+	'14^3': '2744',
+	'(324+329)\\times2014': 1315142
+	
+}
+
+export const reg14 = {
+	'41914+13+50314=': '92241',
+	'25\\times 41': '1025',
+	'531.4-41.35=': '490.05',
+	'2014\\div 5=': '402.8',
+	'\\frac{5}{8}=\\textrm{___%(mixed number)}': '62 1/2',
+	'51232014\div11 \\textrm{ has a remainder of }': '9',
+	'5\\frac{1}{4}+2\\frac{2}{3}=\\textrm{___(mixed number)}': '7 11/12',
+	'18\\times15+15\\times 32=': '750',
+	'5\\times(3-20+1\div 4)=': '-20',
+	'5314+531+53+5': 5939,
+	'\\textrm{2 gallons + 3 quarts + 1 pint = ___ cups}': '46',
+	'31\\% \\textrm{ of }31=': ['9.61','961/100','9 61/100'],
+	'7\\times\\frac{7}{11}=': '4 5/11',
+	'8\\frac{3}{5}-5\\frac{3}{8}=': '3 9/40',
+	'63\\times 44=': '2772',
+	'\\textrm{MMCDXV=____(arabc NUmbers)}': '2415',
+	'(\\frac{9}{11})^3=': '729/1331',
+	'\\$18.00 \\textrm{is 40\\% of $___}': '45.00',
+	'\\textrm{the multiplicative inverse of }-1\\frac{5}{7}\\textrm{ is ___}': '-7/12',
+	'532014\\div415': '1282'
+	
+}
+
+export const state14 = {
+	'519+2014=': '2533',
+	'124\\times 15=': '1860',
+	'51.9-20.14': '31.76',
+	'201\\div4=': '50.25',
+	'6.25\\%=\\textrm{__ (proper fraction)}': '1/16',
+	'61\\times 16=': '976',
+	'23^2=': '529',
+	'5+1\\times 9\\div2^0-14=': '0',
+	'5\\frac{1}{9}+2\\frac{1}{4}': ['265/36','7 13/36'],
+	'4102-915-2014-519=': 4682,
+	'546738\\div 11\\textrm{ has a remainder of ___}': '5',
+	'7\\frac{4}{5}-4\\frac{1}{2}=': ['3.3','33/10','3 3/10'],
+	'14^3=': '2744',
+	'\textrm{which is smaller, }\\frac{5}{12}\\textrm{ or }0.45?': '5/12',
+	'Thenumbder of prime factors of 210 is': '4',
+	'37.5\\% \\textrm{ of \\$24.16 is \\$___}': '9.06',
+	'\\textrm{MCDXCII= ___(Arabic Numbers)}': '1492',
+	'\\textrm{The multiplicative inverse of 1.2 is ___}': '5/6',
+	'4+7+10+13+...+34+37=': '246',
+	'210\\times45\\times19=': 179550
+	
+	
+}
